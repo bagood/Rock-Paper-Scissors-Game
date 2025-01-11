9b489b41f39f1c7891bae42e72440860640d516e
Ariabagus Mustikoatmodjo <ariabagus@Ariabaguss-MacBook-Air.local>
2025-01-11 08:54:35 +0700

    this commit combines the first, second, and third commit

---
+++ normalFile.py
@@ -0,0 +1,8 @@
+print('This is the original first line')
+print('This is the original second line')
+
+print('This is the first line one the second commit')
+print('This is the second line one the second commit')
+
+print('This is the first line one the third commit')
+print('This is the second line one the third commit')
